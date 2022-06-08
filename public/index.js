eb8cf1f1564bcff03a1dd2f044c3012f3f037f91
jwaltech <jamesduncanwalter@gmail.com>
2022-06-07 17:45:42 -0700

    functionality changes

--- public/index.js
+++ public/index.js
@@ -1,14 +1,59 @@
-
-function insertreview() {
-  var context = { title: "Sample title reviewing course", text: "Sample review text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." };
-  var review = Handlebars.templates.review(context); 
+function insertreview(title,text) {
+  var context = { title: title, text: text };
+  var review = Handlebars.templates.review(context);
   var container = document.getElementsByClassName("review-container");
   container[0].insertAdjacentHTML("beforeend", review);
 }
 
 
 window.addEventListener('DOMContentLoaded', function () {
-	insertreview()
-	insertreview()
-	insertreview()
+
 });
+
+function createreview(){
+
+  var review_text = document.getElementById("review-text").value;
+  var review_title = document.getElementById("review-title").value;
+
+  if(review_text == "" || review_title == ""){
+    alert("please dont leave a text field empty");
+  }else{
+    insertreview(review_title, review_text);
+    document.getElementById("review-text").value = "";
+    document.getElementById("review-title").value = "";
+
+
+
+    loadServer(review_title, review_text);
+
+    //const newReview = {
+      //title: "The title of the review",
+      //text: "Text to be formatted into a review the same way we did in assignment 5."
+    //};
+
+
+    //const jsonString = JSON.stringify(newReview);
+
+    //fs.writeFile('./reviewData.json', jsonString , err => {
+    //   if(err){
+      //   console.log("Error");
+      // } else {
+        //  console.log("Success");
+      // }
+    //});
+  }
+
+}
+
+function loadServer(title ,text){
+  payload = { title : title,
+              text : text };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST','/');
+  xhr.setRequestHeader('Content-Type','application/json');
+  console.log(payload);
+  payloadjson = JSON.stringify(payload);
+  console.log(payloadjson);
+  xhr.send(payloadjson);
+}
--- server.js
+++ server.js
@@ -9,14 +9,47 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+var reviewData = require('./reviewData.json');
+
 var path = require('path');
 
+const StringDecoder = require('string_decoder').StringDecoder;
+
 app.get('/', function (req, res) {
-  res.status(200).render("classview"); 
+  res.status(200).render("classview", {
+    title: "reviewer",
+    reviews: reviewData
+  });
+
+
+
 });
 
 app.use(express.static('public'));
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
+});
+
+app.post('/',function(req, res){
+  let buffer = "";
+  let decoder = new StringDecoder('utf-8');
+
+  req.on('data', function(data){
+    //console.log(data);
+    buffer += decoder.write(data);
+    //console.log(buffer);
+    let newObject = JSON.parse(buffer);
+    reviewData.push(newObject);
+
+    jstring = JSON.stringify(reviewData);
+    fs.writeFile('./reviewData.json',jstring, err=>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Success");
+      }
+
+    });
+  });
 });
